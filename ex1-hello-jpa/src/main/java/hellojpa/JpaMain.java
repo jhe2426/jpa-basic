b489e3fd7fdb5060bd9ade89ee85e14c1828be85
jhe2426 <jhe2426@naver.com>
2026-07-01 23:36:28 +0900

    영속성 컨텍스트1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,38 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
+/*
+    EntityManagerFactory는 사용자의 요청이 올 때마다 EntityManager를 생성하고 EntityManager는 데이터베이스의 커넥션을 사용하여
+    디비를 사용함
+
+    영속성 컨텍스트
+    - 엔티티를 영구 저장하는 환경이라는 의미
+    - EntityManager.persist(member)는 디비에 데이터를 저장한다는 의미가 아니라 영속성 컨텍스트를 통해서 해당 데이터인 엔티티를 영속한다는 의미임
+        persist() 메서드는 디비에 데이터를 저장하는 것이 아닌 영속성 컨텍스에 저장한다.
+    - 영속성 컨텍스트는 논리적인 개념이며 눈에 보이지 않다.
+    - 엔티티 매니저를 통해서 영속성 컨텍스에 접근할 수 있다.
+
+    엔티티의 생명주기
+    - 비영속: 영속성 컨텍스트와 전혀 관계가 없는 새로운 상태
+        객체를 생성한 상태
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+    - 영속: 영속성 컨텍스트에 관리되는 상태
+        persist(엔티티)가 실행되게 되면 해당 엔티티의 영속 상태가 됨
+            em.persist(member);
+
+    - 준영속: 영속성 컨텍스트에 저장되었다가 분리된 상태
+        아래와 같이 detach()메서드를 사용해서 회원 엔티티를 영속성 컨텍스트에서 분리를 준영속 상태라고 함
+            em.detach(member);
+
+    - 삭제: 삭제된 상태
+        em.remove(member);
+            영속 상태의 엔티티에 em.remove()를 호출하면 해당 엔티티는 삭제 상태로 전이된다.
+            이 상태에서는 즉시 데이터베이스에서 삭제되는 것이 아니라, 트랜잭션이 커밋되는 시점에
+            실제로 DELETE SQL이 실행되어 데이터베이스에서 제거된다.
+*/
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -105,7 +137,7 @@ public class JpaMain {
             - SQL을 추상화해서 특정 데이터베이스 SQL에 의존하지 않음
             - JPQL을 한마디로 정의하면 객체 지향 SQL
 */
-        try {
+/*        try {
 //            Member findMember = em.find(Member.class, 1l);
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)
@@ -115,6 +147,24 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.println("member.getName() = " + member.getName());
             }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+        try {
+            // Member 엔티티는 비영속 상태
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속 상태
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
 
             tx.commit();
         } catch (Exception e) {
