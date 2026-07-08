31a5673bc6e69967488accb2d1be34aca0cf09b5
jhe2426 <jhe2426@naver.com>
2026-07-08 16:15:15 +0900

    실전 예제1 - 요구사항 분석과 기본 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    /*
+        해당 엔티티는 객체 설계를 테이블 설계에 맞춘 방식임
+        테이블의 외래키를 객체에 그대로 가져옴
+            @Column(name = "MEMBER_ID")
+            private Long member
+        객체 그래프 탐색이 불가능
+            객체 그래프 탐색: 객체가 참조하고 있는 다른 객체를 점(.)으로 계속 따라가면서 조회하는 것
+                객체가 가진 참조를 따라 다른 객체로 이동하면서 데이터를 조회하는 것
+    */
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
