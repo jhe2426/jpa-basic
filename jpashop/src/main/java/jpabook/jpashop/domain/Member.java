97298da785f8837c31341a5bc63e24ca27b97dd4
jhe2426 <jhe2426@naver.com>
2026-07-12 20:00:44 +0900

    실전 예제2 - 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -2,6 +2,9 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -12,6 +15,10 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    // 보편적으로는 Member에 orders에 대한 정보를 저장하여 양방향으로 매핑한다는 것은 설계를 어렵게 한 것이다. 예제를 통해서 양방향 매핑을 한 것
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
